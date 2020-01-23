e2298c2e828671ef7e62318b80535b5a25405dfb
Drachenbauer <47259059+Drachenbauer@users.noreply.github.com>
2020-01-23 21:28:45 +0100

    Update GlazedTerracottaTileBlock.java

--- src/main/java/drachenbauer32/flattilesmod/blocks/GlazedTerracottaTileBlock.java
+++ src/main/java/drachenbauer32/flattilesmod/blocks/GlazedTerracottaTileBlock.java
@@ -1,6 +1,41 @@
 package drachenbauer32.flattilesmod.blocks;
 
-public class GlazedTerracottaTileBlock
+import net.minecraft.block.Block;
+import net.minecraft.block.BlockState;
+import net.minecraft.block.HorizontalBlock;
+import net.minecraft.item.BlockItemUseContext;
+import net.minecraft.item.DyeColor;
+import net.minecraft.state.DirectionProperty;
+import net.minecraft.state.StateContainer;
+import net.minecraft.util.Direction;
+import net.minecraft.util.Rotation;
+
+public class GlazedTerracottaTileBlock extends DyableTileBlock
 {
     
+    public static final DirectionProperty FACING = HorizontalBlock.HORIZONTAL_FACING;
+    
+    public GlazedTerracottaTileBlock(String name, DyeColor p_i48290_1_, Properties properties)
+    {
+        super(name, p_i48290_1_, properties);
+        this.setDefaultState(this.stateContainer.getBaseState().with(FACING, Direction.NORTH));
+    }
+    
+    @Override
+    protected void fillStateContainer(StateContainer.Builder<Block, BlockState> builder)
+    {
+        builder.add(FACING);
+    }
+    
+    @Override
+    public BlockState getStateForPlacement(BlockItemUseContext context)
+    {
+        return this.getDefaultState().with(FACING, context.getPlacementHorizontalFacing().getOpposite());
+    }
+    
+    @Override
+    public BlockState rotate(BlockState state, Rotation rot)
+    {
+        return state.with(FACING, rot.rotate(state.get(FACING)));
+    }
 }
